82dce1b96a2a6ff8a91093f016d3d89cd70d61d5
Maksim Kobylyuk <100240849+KobylyukM21@users.noreply.github.com>
2023-03-16 15:46:54 -0400

    Add files via upload

---
+++ MSK Ticketing System/customer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>MSK Consulting</title>
+	<link rel="stylesheet" href="./style/css/employee.css">
+</head>
+
+<body>
+	<h2>Welcome MSK Customer | Please Login</h2>
+	<form action="login.php" method="post">
+		<label for="username">Username:</label>
+		<input type="text" id="username" name="username"><br><br>
+		<label for="password">Password:</label>
+		<input type="password" id="password" name="password"><br><br>
+		<input type="submit" value="Login">
+	</form>
+</body>
+
+</html>
---
+++ MSK Ticketing System/database/config.php
@@ -0,0 +1,38 @@
+<?php
+// Start the session
+session_start();
+
+// Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+	// Get the form data
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+
+	// Connect to the database
+	$conn = mysqli_connect("localhost", "root", "", "ticketsystem");
+
+	// Check if the connection was successful
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+	// Prepare the SQL statement to retrieve the user with the given username and password
+	$sql = "SELECT * FROM employee WHERE username='$username' AND password='$password'";
+
+	// Execute the SQL statement
+	$result = mysqli_query($conn, $sql);
+
+	// Check if a row was returned
+	if (mysqli_num_rows($result) == 1) {
+		// Valid credentials, redirect to the homepage.php page
+		$_SESSION["username"] = $username; // Store the username in a session variable
+		header("Location: homepage.php");
+		exit();
+	} else {
+		// Invalid credentials, display an error message
+		$error = "Invalid login credentials. Please try again.";
+	}
+}
+
+?>
